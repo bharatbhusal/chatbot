e6d3fddb878e9daf9962085224282ee974389a2f
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-09-18 21:03:19 +0530

    basic tg bot ready

---
+++ telegram_bot/main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os 
+import telebot
+
+load_dotenv()
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+bot.infinity_polling()
