142ee6e2b2f475a69ab422b6b54bef3f515d7be2
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-27 00:52:04 +0530

    MesageHandler

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,17 +1,19 @@
 # main.py
 from config import load_environment, get_bot
-from handlers import get_today, handle_user_input, reply_to_user_message
+from handlers import MessageHandler
 
 def main():
     load_environment()
     bot = get_bot()
+    message_handler = MessageHandler()
 
 
     @bot.message_handler(func=lambda message: True)
     def handle_messages(message):
-        user_input = message.text
-        reply_message = handle_user_input(user_input)
-        reply_to_user_message(bot, message, reply_message)
+        user_input = message.text.lower()
+        if "leca" in user_input:
+            reply_message = message_handler.handle_user_input(user_input)
+            message_handler.reply_to_user_message(bot, message, reply_message)
 
     print("The bot is live")
     bot.infinity_polling()
